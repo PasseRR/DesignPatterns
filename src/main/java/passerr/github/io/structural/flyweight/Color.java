8804c4274c37f0f97e2698f86cffa9335f6664aa
PasseRR <xie__hai@sina.com>
2017-08-07 15:21:36 +0800

    flyweight

--- src/main/java/passerr/github/io/structural/flyweight/Color.java
+++ src/main/java/passerr/github/io/structural/flyweight/Color.java
@@ -1,17 +1,16 @@
 package passerr.github.io.structural.flyweight;
 
+import lombok.Getter;
+
 /**
  * @author xiehai1
  * @date 2017/08/07 13:44
  * @Copyright(c) gome inc Gome Co.,LTD
  */
 public class Color {
+    @Getter
     private String color;
     public Color(String color){
         this.color = color;
-    }
-
-    public String getColor() {
-        return color;
     }
 }
