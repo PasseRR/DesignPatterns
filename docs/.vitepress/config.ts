3390c02dd46c77d14e94d0f33a04e1b79f563d71
PasseRR <xie__hai@sina.com>
2023-08-25 22:39:38 +0800

    设计模式基于vitepress重构

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,83 @@
+import {defineConfig} from 'vitepress'
+import sidebars from './sidebars';
+
+// 基础路径
+const base = '/DesignPatterns',
+    // google 分析
+    google = 'G-1L1DPX3PFD',
+    // 百度统计
+    baidu = '8537fef7fc5f543ba1ad52b802978f07';
+
+export default defineConfig({
+    title: "设计模式",
+    description: "A VitePress Site",
+    lastUpdated: true,
+    base: base,
+    srcExclude: ['**/README.md'],
+    head: [
+        // google分析脚本
+        [
+            'script',
+            {async: '', src: `https://www.googletagmanager.com/gtag/js?id=${google}`}
+        ],
+        // google、百度统计分析
+        [
+            'script',
+            {},
+            `var isProductEnv = window && window.location && window.location.host
+                && window.location.host.indexOf('127.0.0.1') === -1
+                && window.location.host.indexOf('localhost') === -1
+                && window.location.host.indexOf('192.168.') === -1;
+             if(isProductEnv) {
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${google}');
+                
+                var _hmt = _hmt || [];
+                (function () {
+                    var hm = document.createElement("script");
+                    hm.src = "https://hm.baidu.com/hm.js?${baidu}";
+                    var s = document.getElementsByTagName("script")[0];
+                    s.parentNode.insertBefore(hm, s);
+                })();
+                _hmt.push(['_setAccount', '${baidu}']);
+                _hmt.push(['_trackPageview', window.location]);
+                _hmt.push(['_setAutoPageview', true]);
+             }`
+        ]
+    ],
+    sitemap: {
+        hostname: `https://www.xiehai.zone`,
+        lastmodDateOnly: false,
+        // @ts-ignore
+        transformItems(items) {
+            // @ts-ignore
+            return items.map(it => it.url = `${base}/${it.url}`);
+        }
+    },
+    themeConfig: {
+        nav: null,
+        sidebar: sidebars(),
+        search: {
+            provider: 'local'
+        },
+        lastUpdated: {
+            text: '最后更新'
+        },
+        editLink: {
+            pattern: 'https://github.com/PasseRR/DesignPatterns/edit/main/docs/:path',
+            text: 'Edit this page on GitHub'
+        },
+        logo: '/logo.jpg',
+        outline: {
+            level: [2, 6],
+            label: '目录'
+        },
+        langMenuLabel: '菜单',
+        returnToTopLabel: '回到顶部'
+    },
+    markdown: {
+        lineNumbers: true
+    }
+});
---
+++ docs/.vitepress/sidebars.ts
@@ -0,0 +1,47 @@
+export default function sidebars() {
+    return [{
+        text: '创建型模式',
+        collapsed: false,
+        base: 'creational',
+        link: '/index',
+        items: [
+            {text: '单例模式', link: '/singleton'},
+            {text: '原型模式', link: '/prototype'},
+            {text: '抽象工厂模式', link: '/abstract-factory'},
+            {text: '工厂方法模式', link: '/factory-method'},
+            {text: '构造器模式', link: '/builder'},
+        ]
+    }, {
+        text: '行为型模式',
+        collapsed: false,
+        base: 'behavioural',
+        link: '/index',
+        items: [
+            {text: '责任链模式', link: '/chain-of-responsibility'},
+            {text: '命令模式', link: '/command'},
+            {text: '解释器模式', link: '/interpreter'},
+            {text: '迭代器模式', link: '/iterator'},
+            {text: '中介者模式', link: '/mediator'},
+            {text: '备忘录模式', link: '/memento'},
+            {text: '观察者模式', link: '/observer'},
+            {text: '状态模式', link: '/state'},
+            {text: '策略模式', link: '/strategy'},
+            {text: '模版方法模式', link: '/template-method'},
+            {text: '访问者模式', link: '/visitor'},
+        ]
+    }, {
+        text: '结构型模式',
+        collapsed: false,
+        base: 'structural',
+        link: '/index',
+        items: [
+            {text: '组合模式', link: '/composite'},
+            {text: '外观模式', link: '/facade'},
+            {text: '适配器模式', link: '/adapter'},
+            {text: '桥接模式', link: '/bridge'},
+            {text: '装饰器模式', link: '/decorator'},
+            {text: '享元模式', link: '/flyweight'},
+            {text: '代理模式', link: '/proxy'},
+        ]
+    }];
+};
